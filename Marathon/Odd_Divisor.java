357c04bd345eac7e178be7222f0b20fd8af1efef
Bhuban <71319456+bhuban3@users.noreply.github.com>
2025-01-05 00:08:59 +0600

    Update Odd_Divisor.java

--- Marathon/Odd_Divisor.java
+++ Marathon/Odd_Divisor.java
@@ -1,1 +1,25 @@
 
+import java.util.Scanner;
+
+public class Odd_divisor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 1; i <= t; i++) {
+            long n = sc.nextLong();
+            if (n % 2 > 1) {
+                System.out.println("YES");
+                return;
+            }
+            while (n % 2 == 0) {
+                n /= 2;
+            }
+            if (n > 1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+}
